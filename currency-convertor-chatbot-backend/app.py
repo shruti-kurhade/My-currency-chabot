5e18c0c5d59a016f4c0bf25f7c5a539a0333c123
shruti-kurhade <kurhadeshruti1@gmail.com>
2026-01-30 18:10:44 +0530

    Update app.py

--- currency-convertor-chatbot-backend/app.py
+++ currency-convertor-chatbot-backend/app.py
@@ -3,16 +3,22 @@ import requests
 
 app = Flask(__name__)
 
-# Just for browser testing
 @app.route('/', methods=['GET'])
 def home():
     return "Currency Converter Webhook is Running."
 
-# Dialogflow webhook
 @app.route('/', methods=['POST'])
 def index():
     data = request.get_json()
 
+    intent_name = data['queryResult']['intent']['displayName']
+
+    
+    if intent_name == "supported-currency":
+        return jsonify({
+            "fulfillmentText": "I support most international currencies including US Dollar (USD), Indian Rupee (INR), Euro (EUR), British Pound (GBP), Japanese Yen (JPY), Australian Dollar (AUD), Canadian Dollar (CAD) and many others."
+            })
+    
     try:
         source_currency = data['queryResult']['parameters']['unit-currency']['currency']
         amount = data['queryResult']['parameters']['unit-currency']['amount']
@@ -21,14 +27,13 @@ def index():
         cf = fetch_conversion_factor(source_currency, target_currency)
         final_amount = round(amount * cf, 2)
 
-        response = {
-            "fulfillmentText": f"{amount} {source_currency} is {final_amount} {target_currency}"
-        }
-        return jsonify(response)
-
-    except Exception as e:
         return jsonify({
-            "fulfillmentText": "Sorry, I couldn't process that conversion."
+            "fulfillmentText": f"{amount} {source_currency} is {final_amount} {target_currency}"
+        })
+
+    except Exception:
+        return jsonify({
+            "fulfillmentText": "Please enter a valid currency conversion. Example: Convert 10 USD to INR."
         })
 
 
